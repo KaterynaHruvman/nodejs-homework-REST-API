13f2a9fd851b29406540c4c19e4a3cffc086742d
Ekateryna Gruvman <kate.gruvman@ukr.net>
2021-09-28 11:37:05 +0300

    create avatar image

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const createFolderIsNotExist = require("../src/helpers/create-folder");
+const UPLOAD_DIR = process.env.UPLOAD_DIR;
+const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS;
 
 const app = require("../app");
 const { connectMongo } = require("../src/db/conections");
@@ -8,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 const starts = async () => {
   try {
     await connectMongo();
+    await createFolderIsNotExist(UPLOAD_DIR);
+    await createFolderIsNotExist(AVATAR_OF_USERS);
     app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
     });
